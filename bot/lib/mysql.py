d922eabcdedc2e37eedf123f80529e4248e8ae03
Thomas <guitaristtom@users.noreply.github.com>
2020-07-24 21:30:12 -0700

    Apparently I need to put my environment loader in here as well. Ugh...

--- bot/lib/mysql.py
+++ bot/lib/mysql.py
@@ -6,6 +6,18 @@ import os
 from pathlib import Path
 
 class mysql():
+    # Load up the environment variables
+    env_file_name = '.env'
+    env_path = Path('.') / env_file_name
+    load_dotenv(dotenv_path=env_path)
+
+    # Check if .env.local exists, if so, load up those variables, overriding the
+    # previously set ones
+    local_env_file_name = env_file_name + '.local'
+    local_env_path = Path('.') / local_env_file_name
+    if os.path.isfile(local_env_file_name):
+        load_dotenv(dotenv_path=local_env_path, override=True)
+
     def __init__(self):
         self._db = connector.connect(
             host=os.environ['MYSQL_HOST'],
