99a06e367511d63c88aeb11c8e54e10ca21ae494
Thomas <guitaristtom@users.noreply.github.com>
2020-07-27 16:13:35 -0700

    Oops, forgot the key for the preferences table.

--- schema.sql
+++ schema.sql
@@ -7,7 +7,10 @@ SET FOREIGN_KEY_CHECKS=0;
 
 CREATE TABLE `preferences` (
   `guild` bigint(20) NOT NULL COMMENT 'Discord Guild ID',
-  `command_prefix` char(1) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The character that is the prefix of commands'
+  `command_prefix` char(1) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The character that is the prefix of commands',
+  PRIMARY KEY (`guild`),
+  UNIQUE KEY `guild` (`guild`),
+  KEY `index_guild` (`guild`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Specific Guild Preferences';
 
 CREATE TABLE `shiny_list` (
