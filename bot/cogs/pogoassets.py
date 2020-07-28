1e36fae5d69af3cd757330a47fe740f2cb951c6e
Thomas <guitaristtom@users.noreply.github.com>
2020-07-28 01:32:31 -0700

    Taadaa, all the GraphQL work is gone, because IT DOESN'T HAVE SUPPORT FOR WHAT I NEED...

--- bot/cogs/pogoassets.py
+++ bot/cogs/pogoassets.py
@@ -1,31 +1,30 @@
 from discord.ext import commands
+import github
 from lib.mysql import mysql
-from lib.github import github
 import discord
 import os
 
 class PoGoAssets(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.github = github
+        self.github = github.Github(os.environ['GITHUB_ACCESS_TOKEN'])
 
     @commands.command()
     async def run(self, ctx):
-        await ctx.send("github test")
-        query = """
-{
-  repository(owner: "PokeMiners", name: "pogo_assets") {
-    pushedAt
-  }
-  rateLimit {
-    limit
-    cost
-    remaining
-    resetAt
-  }
-}
-        """
-        await ctx.send(self.github().run_query(query))
+        repo = self.github.get_repo("PokeMiners/pogo_assets")
+        # test = repo.create_git_tree([
+        #     github.InputGitTreeElement(
+        #         "Images/Pokemon",
+        #         "100644",
+        #         "blob",
+        #         #content="File created by PyGithub"
+        #     )
+        # ])
+
+        # print(test)
+
+        branch = repo.get_branch("master")
+        await ctx.send(branch)
 
 def setup(client):
     client.add_cog(PoGoAssets(client))
