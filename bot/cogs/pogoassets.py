9e42d83df1785efd533cde01c67b23a93b62dffa
Thomas <guitaristtom@users.noreply.github.com>
2020-07-28 03:32:12 -0700

    Started work on the file name translator

--- bot/cogs/pogoassets.py
+++ bot/cogs/pogoassets.py
@@ -3,6 +3,7 @@ import github
 from lib.mysql import mysql
 import discord
 import os
+import re
 
 class PoGoAssets(commands.Cog):
     def __init__(self, client):
@@ -42,6 +43,23 @@ class PoGoAssets(commands.Cog):
         results = db.execute(query, ['pogo_assets_commit_hash', sha])
         db.close()
 
+    def translate_filename(self, filename):
+        # Convert the tree / path object to a string
+        text = str(filename)
+
+        # If it is a shiny, FOR NOW skip it completely
+        # WIP maybe have a way to log when the shiny is in the game, but not exactly released yet
+        if "_shiny" in text:
+            return
+
+        # Remove the 'pokemon_icon_' from the beginning of the string
+        text = filename[13:]
+
+        # If it now starts with 'pm', it's a special Pokemon
+        if filename[:2] == "pm":
+            print(f"Special Pokemon: {text}")
+
+        print(text)
 
     @commands.command()
     async def run(self, ctx):
@@ -49,15 +67,14 @@ class PoGoAssets(commands.Cog):
         commit_hash = self.get_newest_commit_hash(repo)
         new_commit = self.check_commit_hash(commit_hash)
 
-        if new_commit:
+        if new_commit == False: # NOTE This is hard set to false for testing
             # In case I need to get a different tree_id later:
             # https://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api/61656698#61656698
             # https://api.github.com/repos/PokeMiners/pogo_assets/git/trees/master?recursive=1
             files = repo.get_git_tree("ace98ff9284529e67c1fa3d1548d953596254b6e").tree
 
             for file in files:
-                # print(file.path)
-                pass
+                self.translate_filename(file.path)
 
             self.store_commit_hash(commit_hash)
             await ctx.send("New commit!")
