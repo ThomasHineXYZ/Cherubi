bee915d1b1c122bce8faaa7ffbd4b0f0b05f79fd
Thomas <guitaristtom@users.noreply.github.com>
2020-07-28 01:30:52 -0700

    (In reference to the previous commit as well) I'm committing this so that it is somewhere, just in case I need it later...

--- bot/cogs/pogoassets.py
+++ bot/cogs/pogoassets.py
@@ -1,30 +1,31 @@
 from discord.ext import commands
-import github
 from lib.mysql import mysql
+from lib.github import github
 import discord
 import os
 
 class PoGoAssets(commands.Cog):
     def __init__(self, client):
         self.client = client
-        self.github = github.Github(os.environ['GITHUB_ACCESS_TOKEN'])
+        self.github = github
 
     @commands.command()
     async def run(self, ctx):
-        repo = self.github.get_repo("PokeMiners/pogo_assets")
-        # test = repo.create_git_tree([
-        #     github.InputGitTreeElement(
-        #         "Images/Pokemon",
-        #         "100644",
-        #         "blob",
-        #         #content="File created by PyGithub"
-        #     )
-        # ])
-
-        # print(test)
-
-        branch = repo.get_branch("master")
-        await ctx.send(branch)
+        await ctx.send("github test")
+        query = """
+{
+  repository(owner: "PokeMiners", name: "pogo_assets") {
+    pushedAt
+  }
+  rateLimit {
+    limit
+    cost
+    remaining
+    resetAt
+  }
+}
+        """
+        await ctx.send(self.github().run_query(query))
 
 def setup(client):
     client.add_cog(PoGoAssets(client))
