51d8dfac0220a2ee7d4614ba43e09a026b6507a2
Thomas <guitaristtom@users.noreply.github.com>
2020-07-28 17:42:30 -0700

    Got Pokemon information importing from the pogo assets repo

--- bot/cogs/pogoassets.py
+++ bot/cogs/pogoassets.py
@@ -14,7 +14,7 @@ class PoGoAssets(commands.Cog):
         branch = repo.get_branch("master")
         return branch.commit.sha
 
-    def check_commit_hash(self, sha):
+    def check_commit_hash(self, hash):
         db = mysql()
         query = """
             SELECT value
@@ -25,41 +25,93 @@ class PoGoAssets(commands.Cog):
         db.close()
 
         if len(results) == 0:
-            self.store_commit_hash(sha)
+            self.store_commit_hash(hash)
             return True
 
-        if results[0][0] == sha:
+        if results[0][0] == hash:
             return False
 
         return True
 
-    def store_commit_hash(self, sha):
+    def store_commit_hash(self, hash):
         db = mysql()
         query = """
             INSERT INTO checks (name, value)
             VALUES (%s, %s)
             ON DUPLICATE KEY UPDATE value = values(value);
         """
-        results = db.execute(query, ['pogo_assets_commit_hash', sha])
+        results = db.execute(query, ['pogo_assets_commit_hash', hash])
+        db.close()
+
+    def store_pokemon(self, files):
+        # Open a connection to the database and set the query up
+        db = mysql()
+        query = """
+            INSERT INTO pogo_pokemon (dex, type, isotope, filename, shiny)
+            VALUES (%s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE shiny = values(shiny);
+        """
+
+        # Iterate through each of the Pokemon picture files and store them in
+        # the table
+        for file in files:
+            data = self.translate_filename(file.path)
+            db.execute(
+                query, [
+                    data['dex'],
+                    data['type'],
+                    data['isotope'],
+                    data['filename'],
+                    data['Shiny'],
+                ],
+            )
+
+        # Finally close the connection
         db.close()
 
     def translate_filename(self, filename):
         # Convert the tree / path object to a string
         text = str(filename)
 
-        # If it is a shiny, FOR NOW skip it completely
-        # WIP maybe have a way to log when the shiny is in the game, but not exactly released yet
+        # Remove the ".png" at the end
+        text = text[:-4]
+
+        # If it is a shiny, set it as such and remove the "_shiny"
+        shiny = False
         if "_shiny" in text:
-            return
+            text = text[:-6]
+            shiny = True
 
         # Remove the 'pokemon_icon_' from the beginning of the string
-        text = filename[13:]
+        text = text[13:]
+
+        # Split up the text in to the various sections
+        split = text.split("_")
 
         # If it now starts with 'pm', it's a special Pokemon
-        if text[0:2] == "pm":
-            print(f"Special Pokemon: {text}")
+        special = False
+        specialFilename = None
+        if split[0][0:2] == "pm":
+            special = True
+            split[0] = split[0][2:]
+            specialFilename = text
 
-        print(text)
+        # Set up the data for returning
+        data = {}
+        data["dex"] = split[0]
+        data["type"] = split[1]
+
+        if special:
+            data["isotope"] = split[3]
+        elif len(split) == 3:
+            data["isotope"] = split[2]
+        else:
+            data["isotope"] = ""
+
+        data["filename"] = specialFilename
+        data["Shiny"] = shiny
+
+        return data
 
     @commands.command()
     async def run(self, ctx):
@@ -67,14 +119,15 @@ class PoGoAssets(commands.Cog):
         commit_hash = self.get_newest_commit_hash(repo)
         new_commit = self.check_commit_hash(commit_hash)
 
-        if new_commit == False: # NOTE This is hard set to false for testing
+        if not new_commit: # NOTE This is hard set to false for testing
             # In case I need to get a different tree_id later:
             # https://stackoverflow.com/questions/25022016/get-all-file-names-from-a-github-repo-through-the-github-api/61656698#61656698
             # https://api.github.com/repos/PokeMiners/pogo_assets/git/trees/master?recursive=1
             files = repo.get_git_tree("ace98ff9284529e67c1fa3d1548d953596254b6e").tree
 
+            self.store_pokemon(files)
             for file in files:
-                self.translate_filename(file.path)
+                data = self.translate_filename(file.path)
 
             self.store_commit_hash(commit_hash)
             await ctx.send("New commit!")
--- schema.sql
+++ schema.sql
@@ -5,6 +5,17 @@
 -- Disable foreign key checks until all schema has been created.
 SET FOREIGN_KEY_CHECKS=0;
 
+CREATE TABLE `pogo_pokemon` (
+  `dex` int(4) unsigned NOT NULL COMMENT 'Dex ID of the Pokemon',
+  `type` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Type to signify a family difference',
+  `isotope` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The isotype ID of the Pokemon',
+  `filename` varchar(46) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Full file name if needed',
+  `shiny` int(1) unsigned DEFAULT NULL COMMENT 'Boolean for if the shiny is programmed in to the game',
+  PRIMARY KEY (`dex`,`type`,`isotope`),
+  UNIQUE KEY `dex_type_isotope` (`dex`,`type`,`isotope`),
+  KEY `dex` (`dex`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='List of the Pokemon currently programmed in to the game';
+
 CREATE TABLE `preferences` (
   `guild` bigint(20) NOT NULL COMMENT 'Discord Guild ID',
   `command_prefix` char(1) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The character that is the prefix of commands',
