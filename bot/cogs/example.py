a17022df3e6487d4d2fa1486e6b216d520caf4fe
Thomas <guitaristtom@users.noreply.github.com>
2020-08-20 15:56:58 -0700

    Updated the example cog a bit. Added the Nest cog.

--- bot/cogs/example.py
+++ bot/cogs/example.py
@@ -1,17 +1,15 @@
 from discord.ext import commands
+from lib.mysql import mysql
+from lib.rediswrapper import Redis
 import discord
-import os
-
+import lib.embedder
 
 class Example(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    def user_is_author(ctx):
-        return ctx.message.author.id == int(os.environ['BOT_AUTHOR'])
-
     @commands.command()
-    @commands.check(user_is_author)
+    @commands.is_owner()
     async def example(self, ctx):
         await ctx.send('Hoorah!')
 
